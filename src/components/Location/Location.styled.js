c69182db58547d3641207b4035b06273d472fed9
Misha Levko <lvk.misha@gmail.com>
2024-06-08 23:47:44 +0300

    Update Location.styled.js

--- src/components/Location/Location.styled.js
+++ src/components/Location/Location.styled.js
@@ -23,7 +23,7 @@ export const Title = styled.h2`
   margin: 0;
   margin-bottom: 20px;
   text-align: start;
-  color: #37451f;
+  color: #b8860b;
 `;
 
 export const PointContainer = styled.div`
@@ -77,7 +77,7 @@ export const PointTitle = styled.h3`
   margin: 0;
   text-align: center;
   font-size: 1.3em;
-  color: #86603a;
+  color: #b8860b;
 `;
 
 export const BanquetPoint = styled.div`
@@ -93,7 +93,7 @@ export const WeddingTime = styled.span`
   font-style: normal;
   font-size: 2em;
   margin: 0;
-  color: #86603a;
+  color: #b8860b;
   text-align: end;
 `;
 
