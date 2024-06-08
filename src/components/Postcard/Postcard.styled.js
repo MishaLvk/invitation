627b66630c20da37446a299367cf8bdd4d114a11
Misha Levko <lvk.misha@gmail.com>
2024-06-08 23:07:55 +0300

    екн2

--- src/components/Postcard/Postcard.styled.js
+++ src/components/Postcard/Postcard.styled.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export const Fon = styled.div`
   margin: 0;
-  background-color: #f2f8ee;
+  background-color: #fffff0;
   width: 100%;
   height: 100%;
 `;
